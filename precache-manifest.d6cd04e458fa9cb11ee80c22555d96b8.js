cef3a94e4714955813e0bc2bd386d126201ff819
petrpisek <55359905+petrpisek@users.noreply.github.com>
2020-03-15 11:44:30 +0100

    Add files via upload

---
+++ precache-manifest.d6cd04e458fa9cb11ee80c22555d96b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d462656e432c91e5fb68db8d01f428e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3717c489b5fc857629b8",
+    "url": "/static/css/main.d4d7af93.chunk.css"
+  },
+  {
+    "revision": "395f22fd925e6b84317f",
+    "url": "/static/js/2.7e44fdac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7e44fdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3717c489b5fc857629b8",
+    "url": "/static/js/main.39fae6d6.chunk.js"
+  },
+  {
+    "revision": "1be0f4dc1dc12a62d439",
+    "url": "/static/js/runtime-main.c0038a1a.js"
+  }
+]);
